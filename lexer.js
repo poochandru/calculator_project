0b459dc80b26de58267b8dc2f9750fab19921a99
poochandru <poochandru1998@gmail.com>
2025-01-28 19:31:18 -0600

    Create lexer.js

---
+++ lexer.js
@@ -0,0 +1,28 @@
+<script type="text/javascript" src="lexer.js"></script> // Allowing HTML file to access lexer.js functions
+
+const TOKEN_TYPE_LITERAL = 0;
+const TOKEN_TYPE_OPERATOR = 1;
+
+function lexer(text) {
+    let tokens = [];
+    let numBuffer = "";
+
+    for (let char of text) {
+        if ("0123456789.".includes(char)) {
+            numBuffer += char;
+        } else if ("+-*/".includes(char)) {
+            if (numBuffer) {
+                tokens.push({ type: TOKEN_TYPE_LITERAL, data: numBuffer });
+                numBuffer = "";
+            }
+            tokens.push({ type: TOKEN_TYPE_OPERATOR, data: char });
+        }
+    }
+
+    if (numBuffer) tokens.push({ type: TOKEN_TYPE_LITERAL, data: numBuffer });
+
+    return tokens;
+}
+
+// Example usage
+console.log(lexer("5 + 3.14 * 2")); // Should output an array of tokens
