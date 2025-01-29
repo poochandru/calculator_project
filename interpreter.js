8200fe889effd55cef7f20f969b12c31ed835320
poochandru <poochandru1998@gmail.com>
2025-01-28 20:14:59 -0600

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -2,17 +2,31 @@
 
 function interpret(node) {
     if (node instanceof LiteralNode) {
+        console.log(`Interpreting LiteralNode with value: ${node.data}`);
         return parseFloat(node.data);
     }
 
     let left = interpret(node.left);
     let right = interpret(node.right);
 
+    console.log(`Interpreting OperatorNode with operator: ${node.data}`);
+    console.log(`Left value: ${left}, Right value: ${right}`);
+
     switch (node.data) {
-        case "+": return left + right;
-        case "-": return left - right;
-        case "*": return left * right;
-        case "/": return right !== 0 ? left / right : "Error";
-        default: throw new Error("Unknown operator");
+        case "+":
+            return left + right;
+        case "-":
+            return left - right;
+        case "*":
+            return left * right;
+        case "/":
+            if (right === 0) {
+                console.log("Division by zero error!");
+                return "Error";
+            }
+            return left / right;
+        default:
+            throw new Error("Unknown operator");
     }
 }
+
