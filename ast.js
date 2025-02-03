ac171bf6c63007b8df552dd02c78e1bf768d0a38
poochandru <poochandru1998@gmail.com>
2025-02-03 17:05:02 -0600

    Update ast.js

--- ast.js
+++ ast.js
@@ -5,6 +5,7 @@ class LiteralNode {
         this.data = data;
     }
 }
+
 class OperatorNode {
     constructor(data, left, right) {
         this.data = data;
@@ -13,27 +14,35 @@ class OperatorNode {
     }
 }
 
-
 function parseToAST(tokens) {
     console.log(`Parsing tokens: ${JSON.stringify(tokens)}`);  
 
     if (tokens.length === 1) {
-        console.log(`Created LiteralNode for: ${tokens[0].data}`);
         return new LiteralNode(tokens[0].data);
     }
 
     let operatorIndex = -1;
     for (let i = 0; i < tokens.length; i++) {
-        if (tokens[i].type === TOKEN_TYPE_OPERATOR) {
+        if (tokens[i].type === TOKEN_TYPE_OPERATOR && (tokens[i].data === "*" || tokens[i].data === "/")) {
             operatorIndex = i;
             break;
         }
     }
 
-    let left = parseToAST(tokens.slice(0, operatorIndex));
-    let right = parseToAST(tokens.slice(operatorIndex + 1));
+    if (operatorIndex !== -1) {
+        let left = parseToAST(tokens.slice(0, operatorIndex));
+        let right = parseToAST(tokens.slice(operatorIndex + 1));
+        return new OperatorNode(tokens[operatorIndex].data, left, right);
+    }
 
-    console.log(`Created OperatorNode with operator: ${tokens[operatorIndex].data}`);
-    return new OperatorNode(tokens[operatorIndex].data, left, right);
+   
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i].type === TOKEN_TYPE_OPERATOR && (tokens[i].data === "+" || tokens[i].data === "-")) {
+            let left = parseToAST(tokens.slice(0, i));
+            let right = parseToAST(tokens.slice(i + 1));
+            return new OperatorNode(tokens[i].data, left, right);
+        }
+    }
+
+    throw new Error("Invalid expression");
 }
-
